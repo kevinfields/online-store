4d21b12b2a2133003ebed281065ed3166caac7b1
Kevin Fields <kfields31@gmail.com>
2022-07-16 13:02:38 -0500

    search function is a little better

--- src/functions/isSearchResult.js
+++ src/functions/isSearchResult.js
@@ -5,10 +5,23 @@ export default function isSearchResult(search, result) {
   let resultArr = lowerAll(result);
   let searchArr = lowerAll(search);
 
-  if (resultArr === searchArr) {
-    return true;
+  let score = 0;
+  const maxScore = searchArr.length * 3;
+
+
+  for (let i=0; i<searchArr.length; i++) {
+    if (searchArr[i] === resultArr[i]) {
+      score = score + 3;
+    } else if (resultArr.includes(searchArr[i])) {
+      score++;
+    } else {
+      continue;
+    }
   }
 
-  return false;
-
+  if (score / maxScore >= 0.5) {
+    return true;
+  } else {
+    return false;
+  }
 }
