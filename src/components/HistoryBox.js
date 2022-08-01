46946327c9a87a7051c6727f63a6d2ccbc43ce25
Kevin Fields <kfields31@gmail.com>
2022-08-01 08:19:51 -0800

    history box upgrade

--- src/components/HistoryBox.js
+++ src/components/HistoryBox.js
@@ -11,13 +11,27 @@ const HistoryBox = (props) => {
         width: '50vw',
         position: 'fixed',
         left: '25vw',
-        top: '40vh',
+        top: '30vh',
         height: '50vh',
         overflowY: 'scroll',
+        backgroundColor: getColor(props.themeSelect, 'card_background'),
+        border: `3px solid ${getColor(props.themeSelect, 'border')}`,
+        boxShadow: `2px 2px 2px 2px ${getColor(props.themeSelect, 'box_shadow')}`,
       }}
     >
       <Button
         onClick={() => props.onClose()}
+        variant='contained'
+        // sx={{
+        //   float: 'right',
+        //   marginRight: '2vw',
+        //   marginTop: '1vh',
+        // }}
+        sx={{
+          position: 'fixed',
+          left: '62.5vw',
+          top: '32.5vh',
+        }}
       >
         Go Back
       </Button>
@@ -29,17 +43,15 @@ const HistoryBox = (props) => {
             borderRadius: '10px',
             boxShadow: getColor(props.themeSelect, 'box_shadow'),
             width: '50%',
+            marginLeft: '10%',
+            marginTop: '1vh',
+            marginBottom: '1vh',
           }}
         >
           <Typography
             color={getColor(props.themeSelect, 'text')}
           >
-            {item.product}
-          </Typography>
-          <Typography
-            color={getColor(props.themeSelect, 'text')}
-          >
-            {item.quantity}
+            {item.product}  {"("}x{item.quantity}{")"}
           </Typography>
           <Typography
             color={getColor(props.themeSelect, 'text')}
