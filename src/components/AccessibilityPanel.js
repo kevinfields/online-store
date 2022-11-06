b963fd05871a95e9cde80781faaf5c1448b88f01
Kevin Fields <kfields31@gmail.com>
2022-11-06 15:46:43 -0600

    accessibility panel layout update

--- src/components/AccessibilityPanel.js
+++ src/components/AccessibilityPanel.js
@@ -6,17 +6,20 @@ const AccessibilityPanel = (props) => {
   return (
     <div className='accessibility-panel'>
       <h4 className='accessibility-header'>Accessibilty</h4>
-      <Button 
-        onClick={() => props.adjustTextSize(true)}
-      >
-        Make Text Bigger
-      </Button>
-      <Button 
-        onClick={() => props.adjustTextSize(false)}>
-        Make Text Smaller
-      </Button>
+      <div className='accessibility-row'>
+        <Button 
+          onClick={() => props.adjustTextSize(true)}
+        >
+          Make Text Bigger
+        </Button>
+        <Button 
+          onClick={() => props.adjustTextSize(false)}>
+          Make Text Smaller
+        </Button>
+      </div>
       <Button
         variant='contained'
+        color='error'
         className='close-accessibility-button'
         onClick={() => props.onClose()}
       >
